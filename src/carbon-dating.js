5525f5442a0bcae2950927df8fb942ae05755c69
Gar <gar@tut.by>
2021-03-06 16:23:58 +0300

    fix: solve Cats Carbon Team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,22 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(str) {
+  //throw new CustomError('Not implemented');
+  if(typeof str !== 'string' || (str == null || isNaN(str))){
+   return false;
+  }
+  
+  str = +str;
+  if(str <= 0 || str > MODERN_ACTIVITY){
+    return false;
+  }
+  let res = 0;
+  if (!isFinite(str)){
+    res = false;
+	}
+  
+  res = Math.ceil(Math.log((MODERN_ACTIVITY / str)) / (0.693 / HALF_LIFE_PERIOD));
+  
+  return res;
+}
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  //throw new CustomError('Not implemented');
+  if (!Array.isArray(arr) || arr.length == 0){
+    return false;
+  }
+  
+  let arrNames = [];
+  let res = '';
+  for(let name of arr){
+  	if (typeof name == 'string'){
+    	let trimmed = name.trim();
+      arrNames.push(trimmed[0].toUpperCase());
+     }
+   }
+   arrNames = arrNames.sort();
+   res = arrNames.join('');   
+ 	return res;
 };
