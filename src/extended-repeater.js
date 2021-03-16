48b37a1c58ba3cd636a3e47512dc96fe09b4ed99
Gar <gar@tut.by>
2021-03-16 18:20:40 +0300

    fix: solve chain and repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, obj) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let substr = '' + str;
+  let addSubstr = '' + obj.addition;
+  if(obj.addition === undefined){
+    addSubstr = '';
+  }
+  let addRepeat = obj.additionRepeatTimes;
+  if(obj.additionRepeatTimes === undefined){
+    addRepeat = 1;
+  }
+  let addSepar = obj.additionSeparator;
+  let times = obj.repeatTimes;
+  if(obj.repeatTimes === undefined){
+    times = 1;
+  }
+  let separ = obj.separator;
+  let addSum = '';
+  let res = '';
+  add(addSubstr, addRepeat, addSepar);
+  function add(addSubstr, addRepeat, addSepar = '|') {
+    addSum = addSubstr + addSepar;
+    addSum = addSum.repeat(addRepeat - 1);
+    addSum = addSum + addSubstr;
+    return addSum;
+  }
+  result(str, times, separ);
+  function result(str, times, separ = '+'){
+  	let res2 = str + addSum + separ;
+    res = res2.repeat(times - 1);
+    res = res + str + addSum;
+    return res;
+  }
+   
+ return res; 
+  
 };
   
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  str: '',
+  res: '',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //throw new CustomError('Not implemented');
+    let arr = this.str.split('~~');
+    return arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //throw new CustomError('Not implemented');
+    if(this.str.length > 0){
+      let arr = this.str.split('~~');
+      arr.push(`( ${value} )`);
+      this.str = arr.join('~~');
+    }
+    else{
+    	this.str = `( ${value} )`;
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arr = this.str.split('~~');
+    if (!Number.isInteger(position) || position > arr.length){
+      this.str = '';
+      throw new Error('Wrong number!');
+    ;
+  }
+   	arr.splice(position - 1, 1);
+    this.str = arr.join('~~');
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //throw new CustomError('Not implemented');
+    let arr = this.str.split('~~');
+    arr = arr.reverse();
+    this.str = arr.join('~~');
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    this.res = this.str;
+    this.str = '';
+    return this.res; 
   }
 };
 
